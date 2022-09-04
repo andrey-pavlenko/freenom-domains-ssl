30bc4b027db4d34cde76b27211ef917804e084f2
Andrey Pavlenko <31999255+andrey-pavlenko@users.noreply.github.com>
2022-09-04 14:00:47 +0300

    feat: Email notifications for expiring domains

--- check-domain-expire/index.ts
+++ check-domain-expire/index.ts
@@ -17,12 +17,6 @@ log4js.configure({
 const logger = log4js.getLogger();
 
 async function getCookie(): Promise<string[]> {
-  return [
-    'WHMCSZH5eHTGhfvzP=49a4tqiq35g0692kbh1libckc7; path=/; HttpOnly',
-    'WHMCSUser=1008653892%3A151ce8214e5c6e7b239f165f365b6f4b261ec55b; expires=Sat, 02-Sep-2023 11:11:13 GMT; Max-Age=31536000; path=/; httponly'
-  ];
-}
-async function getCookie01(): Promise<string[]> {
   const errors: string[] = [];
 
   if (!process.env.FREENOM_LOGIN) {
@@ -39,57 +33,6 @@ async function getCookie01(): Promise<string[]> {
   return (await login(process.env.FREENOM_LOGIN ?? '', process.env.FREENOM_PASSWORD ?? ''))
     .setCookie;
 }
-
-const ds = [
-  {
-    id: 1119857068,
-    name: 'lead.gq',
-    isActive: true,
-    daysLeft: 14,
-    minRenewalDays: 14,
-    renewUrl: 'https://my.freenom.com/domains.php?a=renewdomain&domain=1119857068'
-  },
-  {
-    id: 1121401555,
-    name: 'liorro.tk',
-    isActive: true,
-    daysLeft: 35,
-    minRenewalDays: 14,
-    renewUrl: 'https://my.freenom.com/domains.php?a=renewdomain&domain=1121401555'
-  },
-  {
-    id: 1075424800,
-    name: 'notezz.ml',
-    isActive: true,
-    daysLeft: 36,
-    minRenewalDays: 14,
-    renewUrl: 'https://my.freenom.com/domains.php?a=renewdomain&domain=1075424800'
-  },
-  {
-    id: 1126762819,
-    name: 'agato.tk',
-    isActive: true,
-    daysLeft: 160,
-    minRenewalDays: 14,
-    renewUrl: 'https://my.freenom.com/domains.php?a=renewdomain&domain=1126762819'
-  },
-  {
-    id: 1127137852,
-    name: 'foxy.tk',
-    isActive: true,
-    daysLeft: 167,
-    minRenewalDays: 14,
-    renewUrl: 'https://my.freenom.com/domains.php?a=renewdomain&domain=1127137852'
-  },
-  {
-    id: 1134764844,
-    name: 'qui-quo.tk',
-    isActive: true,
-    daysLeft: 308,
-    minRenewalDays: 14,
-    renewUrl: 'https://my.freenom.com/domains.php?a=renewdomain&domain=1134764844'
-  }
-];
 
 function createSmtpTransport(): ReturnType<typeof createTransport> {
   const errors: string[] = [];
@@ -141,30 +84,25 @@ async function task() {
 
     const smtpTransport = createSmtpTransport();
 
-    // const cookie = await getCookie();
-    const cookie = [
-      'WHMCSZH5eHTGhfvzP=49a4tqiq35g0692kbh1libckc7; path=/; HttpOnly',
-      'WHMCSUser=1008653892%3A151ce8214e5c6e7b239f165f365b6f4b261ec55b; expires=Sat, 02-Sep-2023 11:11:13 GMT; Max-Age=31536000; path=/; httponly'
-    ];
+    const cookie = await getCookie();
     logger.debug('Got login cookie', cookie);
 
     logger.debug('Request renewable domains');
-    // const { domains, errors } = await renewable(
-    //   'https://my.freenom.com/domains.php?a=renewals',
-    //   cookie
-    // );
-    const { domains, errors } = { domains: ds, errors: [] };
+    const { domains, errors } = await renewable(
+      'https://my.freenom.com/domains.php?a=renewals',
+      cookie
+    );
     logger.debug(
       'Got renewable domains',
-      domains.map(({ name, daysLeft }) => ({ name, daysLeft }))
+      (domains ?? []).map(({ name, daysLeft }) => ({ name, daysLeft }))
     );
     if (errors?.length) {
       logger.error('Got renewable domains has errors', errors);
     }
 
-    const domainsSoonExpire = domains.filter((d) => d.daysLeft <= d.minRenewalDays);
+    const domainsSoonExpire = (domains ?? []).filter((d) => d.daysLeft <= d.minRenewalDays);
 
-    if (domainsSoonExpire.length || errors.length) {
+    if (domainsSoonExpire.length || errors?.length) {
       const domains = (domainsSoonExpire ?? []).map(
         ({ name, daysLeft }) => `${name} expires in ${daysLeft} days`
       );
@@ -180,7 +118,7 @@ async function task() {
             subject: 'Domain expiration result',
             text: [
               domains.length ? `Domains are expiring:\n\n${domains.join('\n')}` : '',
-              errors.length ? `Errors when checking domains:\n\n${errors.join('\n')}` : ''
+              errors?.length ? `Errors when checking domains:\n\n${errors.join('\n')}` : ''
             ].join('\n\n')
           },
           (error) => {
@@ -196,24 +134,6 @@ async function task() {
     } else {
       logger.info('No domains about to expire and no errors');
     }
-
-    // if (domainsSoonExpire.length) {
-    //   logger.info(
-    //     'Notification about domains are expiring',
-    //     domainsSoonExpire.map(({ name, daysLeft }) => ({ name, daysLeft }))
-    //   );
-    //   transport.sendMail(
-    //     {
-    //       from: 'andrey.pavlenko@mail.ru',
-    //       to: 'andrey.pavlenko@mail.ru',
-    //       subject: 'test',
-    //       text: 'Soon expire'
-    //     },
-    //     (error) => console.info(error)
-    //   );
-    // } else {
-    // }
-
     logger.info('Check domain expiration: task end');
   } catch (e) {
     logger.fatal('Task failed:', e);
